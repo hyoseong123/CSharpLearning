74f276f3e16d84cbb781774cf977ad02a830ce92
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:26:36 +0900

    ADD Factorial

---
+++ Projects/Factorial_HHS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Factorial_HHS
+{
+    class Program
+    {
+        static int result;
+
+        static void Main(string[] args)
+        {
+            Console.Write("팩토리얼을 알고 싶은 정수를 입력해주세요 (1 ~ 10) : ");
+            
+            string factorialNumberTemp = Console.ReadLine();
+
+            int factorialNumber = int.Parse(factorialNumberTemp);
+
+            result = 1;
+
+            Factorial(factorialNumber);
+
+            Console.WriteLine(result);
+        }
+
+        private static void Factorial(int factorialNumber)
+        {
+            if (factorialNumber == 1)
+            {
+                return;
+            }
+
+            result *= factorialNumber;
+            
+            Factorial(factorialNumber - 1);
+        }
+
+    }
+}
---
+++ Projects/Factorial_HHS/README.md
@@ -0,0 +1,3 @@
+![Factorial_IMAGE1](https://github.com/heewhy/Edu_CSharp/assets/149549575/c1b745b8-7730-4fad-911c-2dc37d92de56)
+![Factorial_IMAGE2](https://github.com/heewhy/Edu_CSharp/assets/149549575/9a44f6a1-aed9-4018-baf1-285f279383e9)
+![Factorial_IMAGE3](https://github.com/heewhy/Edu_CSharp/assets/149549575/9459160e-96b9-4c26-8ce3-740989c10735)
