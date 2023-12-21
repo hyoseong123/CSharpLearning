1797401af0ac7a31783f2a7cca10e9a899be856e
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:27:27 +0900

    ADD HanoiTop

---
+++ Projects/HanoiTop_HHS/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HanoiTop_HHS
+{
+    class Program
+    {
+        private const int count = 5;
+        private static int total = 0;
+
+        private static Stack<int> start = new Stack<int>();
+        private static Stack<int> to = new Stack<int>();
+        private static Stack<int> end = new Stack<int>();
+
+        static void Main(string[] args)
+        {
+            for (int i = count; i > 0 ; i--)
+            {
+                start.Push(i);
+            }
+
+            AllPrintHanoi();
+
+            Hanoi(count, start, end, to);
+
+            Console.WriteLine($"총 이동 횟수는 {total}번 입니다.");
+        }
+
+        private static void Hanoi(int n, Stack<int> startHanoi, Stack<int> endHanoi, Stack<int> toHanoi)
+        {
+            if(n == 1)
+            {
+                int top = startHanoi.Pop();
+
+                endHanoi.Push(top);
+
+                total++;
+
+                AllPrintHanoi();
+            }
+            else
+            {
+                Hanoi(n - 1, startHanoi, toHanoi, endHanoi);
+
+                int top = startHanoi.Pop();
+
+                endHanoi.Push(top);
+
+                total++;
+
+                AllPrintHanoi();
+
+                Hanoi(n - 1,toHanoi,endHanoi,startHanoi);
+            }
+        }
+        
+        private static void AllPrintHanoi()
+        {
+            PrintHanoi("A", start);
+            PrintHanoi("B", to);
+            PrintHanoi("C", end);
+
+            Console.WriteLine();
+        }
+
+        private static void PrintHanoi(string type, Stack<int> stack)
+        {
+            Stack stacktmp = new Stack();
+
+            foreach (var item in stack)
+            {
+                stacktmp.Push(item);
+            }
+
+            Console.Write(type + " : ");
+
+            foreach (var item in stacktmp)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Projects/HanoiTop_HHS/README.md
@@ -0,0 +1,1 @@
+![HanoiTop_IMAGE](https://github.com/heewhy/Edu_CSharp/assets/149549575/d8ff8d29-e9a3-487a-b16b-600f00c9b14e)
