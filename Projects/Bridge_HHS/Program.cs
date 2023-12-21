06fcf87d270e7cfa5817b35537ea0a0a30f52b07
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:26:20 +0900

    ADD Bridge

---
+++ Projects/Bridge_HHS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Bridge_HHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("서쪽과 동쪽의 사이트를 순서대로 입력해주세요 : ");
+
+            string westAndEastTemp = Console.ReadLine();
+            string[] westAndEastArrTemp = westAndEastTemp.Split(' ');
+
+            int westNumber = int.Parse(westAndEastArrTemp[0]);
+            int eastNumber = int.Parse(westAndEastArrTemp[1]);
+            
+            long[,] birdgeArr = new long[30, 30];
+
+            birdgeArr[0, 0] = 1;
+            birdgeArr[1, 1] = 1;
+            birdgeArr[1, 0] = 2;
+
+            // bridgeArr는 1번까지 미리 선언해놨기에 2번부터 배열.
+            for (int i = 2; i < birdgeArr.GetLength(0); i++)
+            {
+                for (int j = 2; j < birdgeArr.GetLength(1); j++)
+                {
+                    birdgeArr[i, j - 1] = birdgeArr[i - 1, j - 2] + birdgeArr[i - 1, j - 1];
+
+                    birdgeArr[i, 0] = i + 1;
+                }
+            }
+            
+            Console.WriteLine(birdgeArr[eastNumber - 1, westNumber - 1]);
+        }
+    }
+}
---
+++ Projects/Bridge_HHS/README.md
@@ -0,0 +1,1 @@
+![Bridge_IMAGE](https://github.com/heewhy/Edu_CSharp/assets/149549575/c774aca2-4a9c-43ba-9b10-5a697c299a92)
