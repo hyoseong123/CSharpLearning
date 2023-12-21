923b70822b8f121402d8a5f95312376b32860d81
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:28:16 +0900

    ADD BalanceWorld

---
+++ Projects/BalanceWorld_HHS/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace BalanceWorld_HHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentenceTmp = Console.ReadLine();
+            char[] sentence = sentenceTmp.ToCharArray();
+
+            int smallTurn = 0;
+            int bigTurn = 0;
+            int smallBracket = 0;
+            int bigBracket = 0;
+
+            foreach (var item in sentence)
+            {
+                if(item == '(')
+                {
+                    smallBracket++;
+                    smallTurn++;
+                }
+
+                if (item == ')' && smallTurn == 0)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                if (item == ')')
+                {
+                    smallBracket--;
+                }
+
+                if (item == '[')
+                {
+                    bigBracket++;
+                    bigTurn++;
+                }
+
+                if (item == ']' && bigTurn == 0)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                if (item == ']')
+                {
+                    bigBracket--;
+                }
+            }
+
+            if (sentence[sentence.Length - 1] == '(' || sentence[sentence.Length - 1] == '[')
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            if (smallBracket == 0 && bigBracket == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ Projects/BalanceWorld_HHS/README.md
@@ -0,0 +1,1 @@
+![BalanceWorld_IMAGE](https://github.com/heewhy/Edu_CSharp/assets/149549575/4ba23f91-bd18-48b2-80b8-6aac84033afe)
