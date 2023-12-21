7a99e4b5b9b592d90a7aab09a43b5a95f394ac99
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:24:46 +0900

    ADD Rectangle

---
+++ Projects/Rectangle_HHS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rectangle_HHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string scaleStringTemp = Console.ReadLine();
+
+            string[] scaleArrTemp = scaleStringTemp.Split(" ");
+
+            int height, width;
+            height = int.Parse(scaleArrTemp[0]);
+            width = int.Parse(scaleArrTemp[1]);
+
+            Console.WriteLine($"사각형의 넓이는 : {height * width} 입니다.");
+        }
+    }
+}
+
---
+++ Projects/Rectangle_HHS/README.md
@@ -0,0 +1,1 @@
+![rectangle](https://github.com/heewhy/Edu_CSharp/assets/149549575/37b2fc2e-82f7-4217-b69b-482278f650e1)
