6d6daf4fa3b1ef9c99c57034ec6e829fecbae5ce
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:24:34 +0900

    ADD Random

---
+++ Projects/Random_HHS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Random_HHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int randomFisrtNumber = random.Next(1,10);
+            int randomSecondNumber = random.Next(100, 123);
+            int randomThirdNumber = random.Next(54, 66);
+
+            Console.WriteLine(randomFisrtNumber);
+            Console.WriteLine(randomSecondNumber);
+            Console.WriteLine(randomThirdNumber);
+        }
+    }
+}
---
+++ Projects/Random_HHS/README.md
@@ -0,0 +1,1 @@
+![random](https://github.com/heewhy/Edu_CSharp/assets/149549575/c961d05e-9333-41d6-9ad5-4dffa5781382)
