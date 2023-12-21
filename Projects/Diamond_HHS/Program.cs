bffda19b3d4f2631c2308d2aed3b113a182c52c7
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:25:47 +0900

    ADD Diamond

---
+++ Projects/Diamond_HHS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Diamond_HHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string scaleNumber = Console.ReadLine();
+
+            // 입력받은 수 int 형으로 변환
+            int scale = int.Parse(scaleNumber);
+            int n = 0;
+
+            for (int i = 0; i < scale * 2; i += 2)
+            {
+                for (int j = scale - 1; j > i/2; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (n = 0; n <= i; n++)
+                {
+                    Console.Write("*"); 
+                }
+
+                Console.WriteLine();
+            }   
+
+            n = n - 2;
+
+            for (int i = n; i > 0; i -= 2)
+            {
+                for (int j = scale - 1; j > i / 2; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+                   Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Projects/Diamond_HHS/README.md
@@ -0,0 +1,3 @@
+![Diamond_Image3](https://github.com/heewhy/Edu_CSharp/assets/149549575/35c7b706-ced0-4070-9971-12a1fb5e65b4)
+![Diamond_Image1](https://github.com/heewhy/Edu_CSharp/assets/149549575/94bebe89-631e-4e75-acf5-fd2cb4515bc9)
+![Diamond_Image2](https://github.com/heewhy/Edu_CSharp/assets/149549575/663e5b11-7f27-4801-982d-f032209b9abb)
