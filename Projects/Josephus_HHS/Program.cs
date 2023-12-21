379be16b55d25aa096253ad88c6393505026eac6
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:29:06 +0900

    ADD Josephus

---
+++ Projects/Josephus_HHS/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Josephus2_HHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arrStringTemp = Console.ReadLine();
+            string[] circleAndSumNumArr = arrStringTemp.Split(" ");
+
+            int circle = int.Parse(circleAndSumNumArr[0]);
+            int sumNum = int.Parse(circleAndSumNumArr[1]);
+
+            int[] quetmp = new int[circle];
+
+            for (int i = 1; i <= circle; i++)
+            {
+                quetmp[i - 1] = i;
+            }
+
+            Queue queue = new Queue(quetmp);
+
+            while (true)
+            {
+                if (queue.Count == 0)
+                {
+                    return;
+                }
+
+                for (int i = 1; i < sumNum; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.Write($"{queue.Dequeue()} ");
+            }
+        }
+    }
+}
---
+++ Projects/Josephus_HHS/README.md
@@ -0,0 +1,1 @@
+![Josephus_IMAGE](https://github.com/heewhy/Edu_CSharp/assets/149549575/d7a14683-4fb1-459c-84ab-92279e3e370b)
