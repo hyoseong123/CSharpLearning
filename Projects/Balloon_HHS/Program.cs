2571334c9054d92bdfb80ca85c09a87bfc103f5a
hyoseong123 <hyoseong123@naver.com>
2023-12-21 15:28:31 +0900

    ADD Balloon

---
+++ Projects/Balloon_HHS/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Balloon_HHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string timesTmp = Console.ReadLine();
+            string setNumbersTmp = Console.ReadLine();
+
+            int times = int.Parse(timesTmp);
+            string[] arr = setNumbersTmp.Split(" ");
+
+            int[] setNumbers = new int[times];
+
+            int dequeTmp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                setNumbers[i] = int.Parse(arr[i]);
+            }
+
+            Queue<int> que = new Queue<int>(setNumbers);
+
+            dequeTmp = que.Dequeue();
+
+            PrintIndex(setNumbers, dequeTmp);
+
+            while (true)
+            {
+                if (que.Count == 0)
+                {
+                    return;
+                }
+
+                if (dequeTmp < 0)
+                {
+                    ReverseQueue(que);
+
+                    for (int j = 1; j < Math.Abs(dequeTmp); j++)
+                    {
+                        que.Enqueue(que.Dequeue());
+                    }
+
+                    dequeTmp = que.Dequeue();
+
+                    PrintIndex(setNumbers, dequeTmp);
+
+                    ReverseQueue(que);
+
+                    continue;
+                }
+
+                for (int j = 1; j < dequeTmp; j++)
+                {
+                    que.Enqueue(que.Dequeue());
+                }
+
+                dequeTmp = que.Dequeue();
+
+                PrintIndex(setNumbers, dequeTmp);
+            }
+        }
+
+        static Queue<int> ReverseQueue(Queue<int> que)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            int count = que.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                stack.Push(que.Dequeue());
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                que.Enqueue(stack.Pop());
+            }
+
+            return que;
+        }
+
+        static void PrintIndex(int[] numbers, int goalNum)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == goalNum)
+                {
+                    Console.Write($"{i + 1} ");
+                }
+            }
+        }
+    }
+}
---
+++ Projects/Balloon_HHS/README.md
@@ -0,0 +1,1 @@
+![Balloon_IMAGE](https://github.com/heewhy/Edu_CSharp/assets/149549575/0011b906-5979-4fab-adf9-d1c82a1e786b)
